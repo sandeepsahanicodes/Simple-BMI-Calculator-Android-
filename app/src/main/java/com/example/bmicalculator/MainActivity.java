80409dc4db4c6449dd1749c15021e48c8917a337
Sandeep Sahani <sandeepsahani76j@gmail.com>
2022-08-18 22:51:58 +0530

    Added comments

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -6,49 +6,51 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.TextView;   // Importing required libraries
 
 public class MainActivity extends AppCompatActivity
 {
-    TextView heightText;
-    EditText heightInput;
+    TextView heightText;   // Creating instance of TextView
+    EditText heightInput; // Creating instance of EditText
     TextView weightText;
     EditText weightInput;
     TextView outputTextView;
-    Button calculateButton;
+    Button calculateButton;  // Creating instance of Button
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        heightText = findViewById(R.id.heightText);
+        setContentView(R.layout.activity_main); // R file holds id's of elements of used resources
+        heightText = findViewById(R.id.heightText); // Initializing TextViews,EditText and Button
         heightInput = findViewById(R.id.heightInput);
         weightText = findViewById(R.id.weightText);
         weightInput = findViewById(R.id.weightInput);
         outputTextView = findViewById(R.id.resultTextView);
         calculateButton = findViewById(R.id.calculateButton);
 
-        calculateButton.setOnClickListener(new View.OnClickListener() {
+        calculateButton.setOnClickListener(new View.OnClickListener()  // Adding click listener on calculate button
+        {
             @Override
             public void onClick(View view)
             {
-//              outputTextView.setText("Button Tapped!");
-                String heightString;
-                String weightString;
+                // Code executes when button is tapped
+                String heightString; // To hold the value of height as string
+                String weightString; // To hold the value of weight as string
 
                 double height;
-                double weight;
-                double BMI;
+                double weight; // Variable to hold weight value as double
+                double BMI;  // Variable to hold BMI value
 
                 heightString = heightInput.getText().toString();
-                weightString = weightInput.getText().toString();
+                weightString = weightInput.getText().toString(); // Getting values of weight and height from EditText as string
 
                 height = Double.parseDouble(heightString);
-                weight = Double.parseDouble(weightString);
-                height = height/100.0;
-                BMI = weight/(height*height);
+                weight = Double.parseDouble(weightString); // Converting weight string to double
+                height = height/100.0; // Converting height value from centimeter to meter
+                BMI = weight/(height*height); // BMI calculation
 
-                outputTextView.setText("Your BMI Value is "+BMI);
+                outputTextView.setText("Your BMI Value is "+BMI); // Setting outputTextView's text
             }
         });
 
